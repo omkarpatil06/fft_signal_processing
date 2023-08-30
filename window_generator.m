2d84adb1b16bb4432118131055c301ab96bc0e6a
Omkar Patil <in.omkap@gmail.com>
2023-08-30 19:10:38 +0200

    Changed window generator

--- window_generator.m
+++ window_generator.m
@@ -1,6 +1,7 @@
 function [window] = window_generator(input)
     % process parameters
     n = 0:input.sample_numbers;
+    input.zeros_symmetric = input.sample_numbers^2;
     window_components = length(input.window_type);
     colour = {'blue', 'red', 'green', 'cyan', 'black', 'magenta'};
     % generating window
@@ -33,21 +34,22 @@ function [window] = window_generator(input)
             case 'Blackman-Harris 4-term'
                 component = 0.36 - 0.49*cos(2*pi*n/length(n)) + 0.14*cos(4*pi*n/length(n)) - 0.01*cos(6*pi*n/length(n));
         end
-    window(i, 1:input.sample_numbers + input.zeros_symmetric + 1) = [component, zeros(1,input.zeros_symmetric)];
+        window(i, 1:input.sample_numbers + 1) = component;
+        window_plot(i, 1:input.sample_numbers + input.zeros_symmetric + 1) = [component, zeros(1,input.zeros_symmetric)];
     end
     % generating a plot for window and their FFT
     if (input.window_plot_status)
         % Finding FFT for plot
-        m = 0:(input.sample_numbers + input.zeros_symmetric - 1)/2;
+        m = 0:(input.sample_numbers + input.zeros_symmetric + 1)/2;
         for i = 1:window_components
-            window_magnitude_fft(i, 1:input.sample_numbers + input.zeros_symmetric) = abs(fft(window(i, 1:input.sample_numbers + input.zeros_symmetric))/(input.sample_numbers + input.zeros_symmetric));
+            window_magnitude_fft(i, 1:input.sample_numbers + input.zeros_symmetric) = abs(fft(window_plot(i, 1:input.sample_numbers + input.zeros_symmetric))/(input.sample_numbers + input.zeros_symmetric));
             window_fft(i, 1:length(m)) = zeros(1, length(m));
             window_fft(i, 1) = 20*log10(window_magnitude_fft(i, 1));
             window_fft(i, 2:length(m)) = 20*log10(2*window_magnitude_fft(i, 2:length(m)));
         end
         % window graph and FFT
         figure;
-        n = 0:input.sample_numbers + input.zeros_symmetric - 1;
+        n = 0:input.sample_numbers;
         subplot(2, 1, 1);   % time series of window function
         hold on
         for i = 1:window_components
@@ -67,7 +69,7 @@ function [window] = window_generator(input)
         legend(input.window_type, 'Location', 'Best');
         hold off
         title(append('Single Sided FFT of Window'));
-        xlim([4, 8]); 
+        xlim([5, 10]); 
         ylim([-180, -30]);
         xlabel('FFT bin (log_{10}(m)');
         ylabel('Amplitude (dB)');
